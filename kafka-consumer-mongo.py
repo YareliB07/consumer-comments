0e263d53d6a1ace74f0cc5fff0e37fef34d89272
YareliB07 <yarelibravo84@gmail.com>
2023-05-22 04:28:26 -0600

    initial code

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -11,7 +11,7 @@ import subprocess
 
 
 # replace here with your mongodb url 
-uri = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://yareli:morchis088@nosql.3mfx8ew.mongodb.net/?retryWrites=true&w=majority"
 
 
 # Create a new client and connect to the server
@@ -27,17 +27,17 @@ uri = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrite
 # Connect to MongoDB and pizza_data database
 
 try:
-    client = MongoClient(uri, server_api=ServerApi('1'))
+    client = MongoClient(uri, username='mongo', password='yareli', authSource='admin')
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    db = client.memes
+    db = client.nosql
     print("MongoDB Connected successfully!")
 except:
     print("Could not connect to MongoDB")
 
 consumer = KafkaConsumer('test',bootstrap_servers=[
-     'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
+     'my-kafka-0.my-kafka-headless.yarelib07.svc.cluster.local:9092'
     ])
 # Parse received data from Kafka
 for msg in consumer:
@@ -46,13 +46,13 @@ for msg in consumer:
     name = record['name']
 
     # Create dictionary and ingest data into MongoDB
-    try:
+    try: 
        meme_rec = {'name':name }
        print (meme_rec)
-       meme_id = db.memes_info.insert_one(meme_rec)
+       meme_id = db.nosql_info.insert_one(meme_rec)
        print("Data inserted with record ids", meme_id)
 
        subprocess.call(['sh', './test.sh'])
 
-    except:
-       print("Could not insert into MongoDB")
+    except Exception as e:
+        print("Could not insert into MongoDB:", e)
